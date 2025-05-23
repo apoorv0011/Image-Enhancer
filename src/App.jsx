c867dd626b12c1cc882d3955d60da8c90c5e94ff
Apoorv <apoorv.pachori0803@gmail.com>
2025-05-23 19:59:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4">
+      <div className="text-center mb-8">
+        <h1 className="text-5xl font-bold text-gray-800 mb-6">
+          AI image Enhancer{" "}
+        </h1>
+        <p className="text-lg text-gray-500">Upload Image</p>
+      </div>
+
+      <Home />
+
+      <div className="text-sm mt-6 text-gray-500">
+        Powered by AP Technovations
+      </div>
+    </div>
+  );
+};
+
+export default App;
